79f46cbcb639fcd6a4eb0c0965a27f7b3d24e12b
Nevtr4l <zlarosavalladares@gmail.com>
2024-01-20 00:34:03 -0500

    Reto 02 - 2023 completado

--- 2023/Reto 02/reto02.js
+++ 2023/Reto 02/reto02.js
@@ -1,2 +1,32 @@
-// Código para el reto 02
-console.log('Hola desde el reto 02');
+// ----------------------------------------------------- //
+// Complejidad Cognitiva: 3 // Puntos conseguidos: 270
+//         Operaciones por segundo: 2875 aprox.
+// ----------------------------------------------------- //
+
+function manufacture(gifts, materials) {
+  return gifts.filter((gift) => {
+    return [...gift].every((char) => {
+      return materials.includes(char)
+    })
+  })
+}
+
+// ----------------------------------------------------- //
+// Complejidad Cognitiva: 3 // Puntos conseguidos: 270
+//         Operaciones por segundo: 2709 aprox.
+// ----------------------------------------------------- //
+
+
+function manufacture(gifts, materials) {
+  let array = []
+
+  for (let gift of gifts) {
+     let push = true
+     for (let letter of gift) {
+        if (!materials.includes(letter)) push = false
+     }
+     if (push) array.push(gift)
+  }
+
+  return array
+}
